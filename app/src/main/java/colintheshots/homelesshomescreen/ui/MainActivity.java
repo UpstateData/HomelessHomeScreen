5b3cbc7567f15836b1f32e32a96d5fc919ffccbb
Colin Lee <colin.lee@thisclicks.com>
2014-06-21 14:06:40 -0500

    Added a drop down menu from the action bar

--- app/src/main/java/colintheshots/homelesshomescreen/ui/MainActivity.java
+++ app/src/main/java/colintheshots/homelesshomescreen/ui/MainActivity.java
@@ -1,10 +1,45 @@
 package colintheshots.homelesshomescreen.ui;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
+import android.widget.SpinnerAdapter;
+
+import colintheshots.homelesshomescreen.R;
 
 /**
  * Created by colintheshots on 6/21/14.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SpinnerAdapter mSpinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.testArray, android.R.layout.simple_spinner_dropdown_item);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        actionBar.setListNavigationCallbacks(mSpinnerAdapter, new ActionBar.OnNavigationListener() {
+            @Override
+            public boolean onNavigationItemSelected(int i, long l) {
+                Log.e("REMOVEME", "It worked!");
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options, menu);
+        return true;
+    }
 }
