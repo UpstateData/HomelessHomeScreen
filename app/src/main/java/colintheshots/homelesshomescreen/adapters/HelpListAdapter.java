edc8b6a334ac825b8df5906c9711b92add4e4f3d
Colin Lee <colin.lee@thisclicks.com>
2014-06-21 19:16:29 -0500

    Add beginning of support for reading JSON files with links to other apps

---
+++ app/src/main/java/colintheshots/homelesshomescreen/adapters/HelpListAdapter.java
@@ -0,0 +1,30 @@
+package colintheshots.homelesshomescreen.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by colintheshots on 6/21/14.
+ */
+public class HelpListAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
--- app/src/main/java/colintheshots/homelesshomescreen/ui/MainActivity.java
+++ app/src/main/java/colintheshots/homelesshomescreen/ui/MainActivity.java
@@ -4,10 +4,21 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.widget.ArrayAdapter;
 import android.widget.SpinnerAdapter;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.ListMultimap;
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import colintheshots.homelesshomescreen.R;
 
@@ -16,24 +27,54 @@ import colintheshots.homelesshomescreen.R;
  */
 public class MainActivity extends ActionBarActivity {
 
+    Map<String, String> menuMap = new HashMap<String, String>();
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
-        SpinnerAdapter mSpinnerAdapter = ArrayAdapter.createFromResource(this,
-                R.array.testArray, R.layout.item_dropdown_spinner);
+        menuMap.put("Benefits","benefits.json");
+        menuMap.put("Health","health.json");
+        menuMap.put("Employment","employment.json");
 
+        SpinnerAdapter mSpinnerAdapter = new ArrayAdapter<String>(this,
+                R.layout.item_dropdown_spinner, menuMap.keySet().toArray(new String[menuMap.keySet().size()]));
 
         ActionBar actionBar = getSupportActionBar();
 
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+//        final String[] strings = getResources().getStringArray(R.array.testArray);
         actionBar.setListNavigationCallbacks(mSpinnerAdapter, new ActionBar.OnNavigationListener() {
             @Override
             public boolean onNavigationItemSelected(int i, long l) {
-                Log.e("REMOVEME", "It worked!");
+//                Log.e("REMOVEME", strings[i]);
+                String key = Iterables.get(menuMap.keySet(), i);
+                StringBuilder json=new StringBuilder();
+
+                try {
+                    Log.e("TEST",menuMap.get(key));
+                    InputStream inputStream = getAssets().open(menuMap.get(key));
+
+                    BufferedReader in=
+                            new BufferedReader(new InputStreamReader(inputStream));
+
+                    String rawJson = "";
+
+                    while ((rawJson=in.readLine()) != null) {
+                        json.append(rawJson);
+                    }
+
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                Map<String,String> map=new HashMap<String,String>();
+
+                map = (Map<String,String>) new Gson().fromJson(json.toString(), map.getClass());
                 return false;
             }
         });
